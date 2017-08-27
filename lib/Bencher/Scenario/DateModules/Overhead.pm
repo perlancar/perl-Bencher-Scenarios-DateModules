879a0a6a95d7f00f8411d2837c60904b3f6bc4da
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-08-27 07:57:07 +0700

    Add scenario: DateModules::Overhead

---
+++ lib/Bencher/Scenario/DateModules/Overhead.pm
@@ -0,0 +1,22 @@
+package Bencher::Scenario::DateModules::Overhead;
+
+# DATE
+# VERSION
+
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Overhead of date/duration modules',
+    module_startup => 1,
+    participants => [
+        {module => 'DateTime'},
+        {module => 'DateTime::Duration'},
+        {module => 'DateTime::Tiny'},
+        {module => 'Time::Moment'},
+        {module => 'Time::Local'},
+        {module => 'Time::Piece'},
+    ],
+};
+
+# ABSTRACT:
